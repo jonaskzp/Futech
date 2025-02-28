1d494f1a9add7aa4752a6442dd2e16d853a89ddc
jonaskzp <jonas.calil@hotmail.com>
2025-02-28 15:33:13 -0300

    final. organizaçao arquivos

--- img/home.js
+++ img/home.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem("auth") !== "true") {
-        window.location.href = "/login.html "; // Redireciona para o login se não estiver autenticado
+        window.location.href = "login.html"; // Redireciona para o login se não estiver autenticado
     }
 });
 function logout() {
     localStorage.removeItem("auth"); // Remove a autenticação
-    window.location.href = "/login.html"; // Redireciona para a página de login
+    window.location.href = "login.html"; // Redireciona para a página de login
 }
